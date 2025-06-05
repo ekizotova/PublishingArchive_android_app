8ba02ddfe2a48a6e7d5f3f6e34b69c4ac937a3b5
izotoeka <izotoeka@cvut.cz>
2025-06-05 12:48:48 +0200

    build success

--- app/src/main/java/cz/cvut/fit/izotoeka/publishingarchive/MyApp.kt
+++ app/src/main/java/cz/cvut/fit/izotoeka/publishingarchive/MyApp.kt
@@ -3,7 +3,6 @@ package cz.cvut.fit.izotoeka.publishingarchive
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
 // todo: don't forget to modify manifest accordingly
 @HiltAndroidApp
 class MyApp : Application()
